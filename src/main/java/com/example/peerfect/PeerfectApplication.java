08031b80b367bfa03838fb84c21a38538d0b5f09
Woo-JonHo <jonghowoo33@gmail.com>
2024-12-02 17:38:40 +0900

    [FEAT] 일차 환경 구성

---
+++ src/main/java/com/example/peerfect/PeerfectApplication.java
@@ -0,0 +1,13 @@
+package com.example.peerfect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerfectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerfectApplication.class, args);
+    }
+
+}
