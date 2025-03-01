c3a6aee2d0d95c56715dd0705f54afa82adad84c
Woo-JonHo <jonghowoo33@gmail.com>
2025-03-01 20:10:07 +0900

    [FIX] 기능 에러 수정 . Detail CurrentDay 추가

--- src/main/java/com/peerfect/DTO/ChallengeDetailDTO.java
+++ src/main/java/com/peerfect/DTO/ChallengeDetailDTO.java
@@ -11,5 +11,6 @@ public class ChallengeDetailDTO {
     private String challengeLevel;
     private String challengeTitle;
     private String challengeReg;
+    private String challengeDay;
     private int challengeCountMember;
 }
